066e4ea22c1bde45a56b783adda148071743ca62
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-11-03 09:57:21 +0700

    feat(guess-number): handling click events

--- guess-number/public/js/main.js
+++ guess-number/public/js/main.js
@@ -1,3 +1,10 @@
 "use strict";
-document.querySelector(".message").textContent = "Correct Number! 🎉";
-document.querySelector(".number").textContent = "100";
+var _a;
+(_a = document
+    .querySelector(".btn-guess")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(guess, typeof guess);
+    if (!guess) {
+        document.querySelector(".message").textContent = "No number";
+    }
+});
--- guess-number/src/main.ts
+++ guess-number/src/main.ts
@@ -1,2 +1,12 @@
-document.querySelector(".message")!.textContent = "Correct Number! 🎉";
-document.querySelector(".number")!.textContent = "100";
+document
+  .querySelector(".btn-guess")
+  ?.addEventListener("click", function (): void {
+    const guess = Number(
+      (document.querySelector(".guess") as HTMLInputElement).value
+    );
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+      document.querySelector(".message")!.textContent = "No number";
+    }
+  });
