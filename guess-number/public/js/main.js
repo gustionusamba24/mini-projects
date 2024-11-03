aa6f30fecc3f82a1dbdc2e0fc4464b5e33b18d9f
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-11-03 10:44:37 +0700

    feat(guess-number): implementing game logic

--- guess-number/public/js/main.js
+++ guess-number/public/js/main.js
@@ -1,10 +1,39 @@
 "use strict";
 var _a;
+const secretNumber = Math.trunc(Math.random() * 10) + 1;
+let score = 10;
+document.querySelector(".number").textContent = secretNumber.toString();
 (_a = document
     .querySelector(".btn-guess")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
     const guess = Number(document.querySelector(".guess").value);
-    console.log(guess, typeof guess);
     if (!guess) {
-        document.querySelector(".message").textContent = "No number";
+        document.querySelector(".message").textContent = "❌ No number";
+    }
+    else if (guess === secretNumber) {
+        document.querySelector(".message").textContent = "🥳 Correct number";
+    }
+    else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = "⬆️ Too high";
+            score--;
+            document.querySelector(".score").textContent = score.toString();
+        }
+        else {
+            document.querySelector(".message").textContent =
+                "😭 You lost the game!";
+            document.querySelector(".score").textContent = "0";
+        }
+    }
+    else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = "⬇️ Too low";
+            score--;
+            document.querySelector(".score").textContent = score.toString();
+        }
+        else {
+            document.querySelector(".message").textContent =
+                "😭 You lost the game!";
+            document.querySelector(".score").textContent = "0";
+        }
     }
 });
--- guess-number/src/main.ts
+++ guess-number/src/main.ts
@@ -1,12 +1,37 @@
+const secretNumber: number = Math.trunc(Math.random() * 10) + 1;
+let score: number = 10;
+document.querySelector(".number")!.textContent = secretNumber.toString();
+
 document
   .querySelector(".btn-guess")
   ?.addEventListener("click", function (): void {
     const guess = Number(
       (document.querySelector(".guess") as HTMLInputElement).value
     );
-    console.log(guess, typeof guess);
 
     if (!guess) {
-      document.querySelector(".message")!.textContent = "No number";
+      document.querySelector(".message")!.textContent = "❌ No number";
+    } else if (guess === secretNumber) {
+      document.querySelector(".message")!.textContent = "🥳 Correct number";
+    } else if (guess > secretNumber) {
+      if (score > 1) {
+        document.querySelector(".message")!.textContent = "⬆️ Too high";
+        score--;
+        document.querySelector(".score")!.textContent = score.toString();
+      } else {
+        document.querySelector(".message")!.textContent =
+          "😭 You lost the game!";
+        document.querySelector(".score")!.textContent = "0";
+      }
+    } else if (guess < secretNumber) {
+      if (score > 1) {
+        document.querySelector(".message")!.textContent = "⬇️ Too low";
+        score--;
+        document.querySelector(".score")!.textContent = score.toString();
+      } else {
+        document.querySelector(".message")!.textContent =
+          "😭 You lost the game!";
+        document.querySelector(".score")!.textContent = "0";
+      }
     }
   });
