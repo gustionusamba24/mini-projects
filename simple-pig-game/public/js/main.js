c8cab4a9695437a99db40409d2a66fe87630682f
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-11-04 19:15:53 +0700

    feat(simple-pig-game): rolling the dice functionality

--- simple-pig-game/public/js/main.js
+++ simple-pig-game/public/js/main.js
@@ -1,7 +1,28 @@
 "use strict";
 const score1El = document.getElementById("score--1");
 const score2El = document.getElementById("score--2");
+const currentScore1El = document.getElementById("current-score--1");
+const currentScore2El = document.getElementById("current-score--2");
 const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn-new");
+const btnRoll = document.querySelector(".btn-roll");
+const btnHold = document.querySelector(".btn-hold");
 score1El.textContent = "0";
 score2El.textContent = "0";
 diceEl.classList.add("hidden");
+let currentScore = 0;
+// Rolling the dice
+btnRoll === null || btnRoll === void 0 ? void 0 : btnRoll.addEventListener("click", function () {
+    // 1. Generating a random dice
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // 2. Display the dice
+    diceEl === null || diceEl === void 0 ? void 0 : diceEl.classList.remove("hidden");
+    diceEl.src = `images/dice-${dice}.png`;
+    // 3. Check for rolled 1: if true then switch to next player
+    if (dice !== 1) {
+        currentScore += dice;
+        currentScore1El.textContent = currentScore.toString();
+    }
+    else {
+    }
+});
--- simple-pig-game/src/main.ts
+++ simple-pig-game/src/main.ts
@@ -1,7 +1,33 @@
 const score1El: Element | null = document.getElementById("score--1");
 const score2El: Element | null = document.getElementById("score--2");
-const diceEl: Element | null = document.querySelector(".dice");
+const currentScore1El: Element | null =
+  document.getElementById("current-score--1");
+const currentScore2El: Element | null =
+  document.getElementById("current-score--2");
+const diceEl: HTMLImageElement | null = document.querySelector(".dice");
+const btnNew: Element | null = document.querySelector(".btn-new");
+const btnRoll: Element | null = document.querySelector(".btn-roll");
+const btnHold: Element | null = document.querySelector(".btn-hold");
 
 score1El!.textContent = "0";
 score2El!.textContent = "0";
 diceEl!.classList.add("hidden");
+
+let currentScore: number = 0;
+
+// Rolling the dice
+btnRoll?.addEventListener("click", function (): void {
+  // 1. Generating a random dice
+  const dice: number = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display the dice
+  diceEl?.classList.remove("hidden");
+  diceEl!.src = `images/dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true then switch to next player
+  if (dice !== 1) {
+    currentScore += dice;
+    currentScore1El!.textContent = currentScore.toString();
+  } else {
+  }
+});
