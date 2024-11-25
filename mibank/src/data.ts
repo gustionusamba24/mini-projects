fb3cbfe8f19d07d5c192b6f7e69ce3f900f92d9d
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-11-25 18:58:38 +0700

    style(04-css-layouts): change button font

---
+++ mibank/src/data.ts
@@ -0,0 +1,57 @@
+type MovementType = "Transfer" | "Loan";
+
+type Account = {
+  owner: string;
+  pin: number;
+  username?: string;
+  movements: number[];
+  interestRate: number;
+  balance?: number;
+  movementType: MovementType[];
+  currency: string;
+  locale: string;
+};
+
+const account1: Account = {
+  owner: "Alucard Budi",
+  pin: 1234,
+  movements: [
+    1000, 2500.5, -3000.45, 15000, -10000.14, 50000, -25000.25, -5000.78, 20000,
+  ],
+  interestRate: 1.2,
+  movementType: [
+    "Transfer",
+    "Loan",
+    "Transfer",
+    "Transfer",
+    "Transfer",
+    "Loan",
+    "Transfer",
+    "Transfer",
+    "Loan",
+  ],
+  currency: "IDR",
+  locale: "id-IDR",
+};
+
+const account2: Account = {
+  owner: "Lunox Sanjaya",
+  pin: 5678,
+  movements: [5000, -2000, 10000, 20000, -7000, 5000, 9500, -10500, 5000],
+  interestRate: 0.5,
+  movementType: [
+    "Transfer",
+    "Transfer",
+    "Loan",
+    "Transfer",
+    "Transfer",
+    "Loan",
+    "Loan",
+    "Transfer",
+    "Transfer",
+  ],
+  currency: "USD",
+  locale: "en-US",
+};
+
+
