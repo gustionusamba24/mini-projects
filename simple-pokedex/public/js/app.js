a1ef755370cad2b68828a320036078b23cddd68e
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-12-01 12:22:12 +0700

    feat(simple-pokedex): finish UI for pokedex

---
+++ simple-pokedex/public/js/app.js
@@ -0,0 +1,3 @@
+"use strict";
+const containerApp = document.getElementById("app");
+const pokemon = 100;
---
+++ simple-pokedex/src/app.ts
@@ -0,0 +1,9 @@
+const containerApp: HTMLElement | any = document.getElementById("app");
+const pokemon: number = 100;
+
+interface IPokemon {
+  id: number;
+  name: string;
+  image: string;
+  type: string;
+}
