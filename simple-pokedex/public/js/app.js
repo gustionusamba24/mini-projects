f972bee3e450630f34a4e46bb9afc990e1ac00fd
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-12-01 15:42:31 +0700

    feat(simple-pokedex): finish fetching and displaying pokemon in the browser

--- simple-pokedex/public/js/app.js
+++ simple-pokedex/public/js/app.js
@@ -1,3 +1,40 @@
 "use strict";
 const containerApp = document.getElementById("app");
 const pokemon = 100;
+const fetchData = () => {
+    for (let i = 1; i <= pokemon; i++) {
+        getPokemon(i);
+    }
+};
+// Fetch data from Pokemon API
+const getPokemon = async (id) => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const pokemonData = await res.json();
+    const pokemonType = pokemonData.types
+        .map((pData) => pData.type.name)
+        .join(", ");
+    const transformedPokemon = {
+        id: pokemonData.id,
+        name: pokemonData.name,
+        image: pokemonData.sprites.front_default,
+        type: pokemonType,
+    };
+    displayPokemon(transformedPokemon);
+};
+// Display Pokemen
+const displayPokemon = (pokemon) => {
+    const output = `
+    <div class="card">
+        <span class="card__id">#${pokemon.id}</span>
+        <img
+            class="card__image"
+            src="${pokemon.image}"
+            alt="Pokemon Image"
+        />
+        <p class="card__name">${pokemon.name}</p>
+        <p class="card__type">${pokemon.type}</p>
+    </div>
+  `;
+    containerApp.insertAdjacentHTML("beforeend", output);
+};
+fetchData();
--- simple-pokedex/src/app.ts
+++ simple-pokedex/src/app.ts
@@ -1,4 +1,4 @@
-const containerApp: HTMLElement | any = document.getElementById("app");
+const containerApp: HTMLElement = document.getElementById("app") as HTMLElement;
 const pokemon: number = 100;
 
 interface IPokemon {
@@ -7,3 +7,47 @@ interface IPokemon {
   image: string;
   type: string;
 }
+
+const fetchData = (): void => {
+  for (let i = 1; i <= pokemon; i++) {
+    getPokemon(i);
+  }
+};
+
+// Fetch data from Pokemon API
+const getPokemon = async (id: number): Promise<void> => {
+  const res: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const pokemonData = await res.json();
+  const pokemonType = pokemonData.types
+    .map((pData: any) => pData.type.name)
+    .join(", ");
+
+  const transformedPokemon = {
+    id: pokemonData.id,
+    name: pokemonData.name,
+    image: pokemonData.sprites.front_default,
+    type: pokemonType,
+  };
+
+  displayPokemon(transformedPokemon);
+};
+
+// Display Pokemen
+const displayPokemon = (pokemon: IPokemon): void => {
+  const output = `
+    <div class="card">
+        <span class="card__id">#${pokemon.id}</span>
+        <img
+            class="card__image"
+            src="${pokemon.image}"
+            alt="Pokemon Image"
+        />
+        <p class="card__name">${pokemon.name}</p>
+        <p class="card__type">${pokemon.type}</p>
+    </div>
+  `;
+
+  containerApp.insertAdjacentHTML("beforeend", output);
+};
+
+fetchData();
