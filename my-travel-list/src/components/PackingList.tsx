d605b7cc47a5d0780c468839fb951a4471d8ca2f
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-04-27 18:55:16 +0700

    feat(my-travel-list): sort the items based on three conditions. There are based on input, description, and packed item

--- my-travel-list/src/components/PackingList.tsx
+++ my-travel-list/src/components/PackingList.tsx
@@ -1,5 +1,6 @@
 import { Item } from "./Item.tsx";
 import { ItemType } from "../schema/ItemType.ts";
+import { useState } from "react";
 
 type PackingListProps = {
   items: ItemType[];
@@ -14,10 +15,28 @@ export const PackingList = ({
   onToggleItem,
   onClearItemList,
 }: PackingListProps) => {
+  const [sortBy, setSortBy] = useState<string>("input");
+
+  let sortedItems: ItemType[] = [];
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(b.packed) - Number(a.packed));
+  }
+
   return (
     <main className="flex flex-col items-center justify-between gap-8 bg-black px-12 py-8">
       <div className="no-scrollbar grid h-[300px] grid-cols-4 content-start justify-center gap-x-9 gap-y-6 overflow-y-scroll">
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             id={item.id}
             description={item.description}
@@ -29,7 +48,11 @@ export const PackingList = ({
         ))}
       </div>
       <div className="flex items-center justify-center gap-x-6">
-        <select className="rounded-sm bg-light-orange px-6 py-2 text-[16px] font-medium uppercase text-white focus:outline-none focus:ring-4 focus:ring-dark-orange">
+        <select
+          className="rounded-sm bg-light-orange px-6 py-2 text-[16px] font-medium uppercase text-white focus:outline-none focus:ring-4 focus:ring-dark-orange"
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
           <option value="input">Sort by input order</option>
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
