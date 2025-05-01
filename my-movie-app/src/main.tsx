f26094edeb7887897539e369ea849d73604e2184
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-05-01 12:36:12 +0700

    feat(my-movie-app): create star rating component

--- my-movie-app/src/main.tsx
+++ my-movie-app/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { App } from "./App.tsx";
+import { StarRating } from "./StarRating";
+// import "./index.css";
+// import { App } from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <StarRating maxRating={5} />
+  </StrictMode>,
 );
