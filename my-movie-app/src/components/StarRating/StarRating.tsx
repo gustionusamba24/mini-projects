3b7ac77517eba746bfc3863bb09b7b652c74f59b
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-05-01 14:57:04 +0700

    style(my-movie-app): finish create layout of an app

---
+++ my-movie-app/src/components/StarRating/StarRating.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { Star } from "./Star";
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+
+const starContainerStyle = {
+  display: "flex",
+};
+
+type StarRatingProps = {
+  maxRating: number;
+  color: string;
+  size: number;
+  onSetRating: (rating: number) => void;
+};
+
+export const StarRating = ({
+  maxRating = 3,
+  color = "#fcc419",
+  size = 48,
+  onSetRating,
+}: StarRatingProps) => {
+  const [rating, setRating] = useState<number>(0);
+  const [tempRating, setTempRating] = useState<number>(0);
+
+  const handleRating = (rating: number) => {
+    setRating(rating);
+    onSetRating(rating);
+  };
+
+  const handleTempRating = (rating: number) => {
+    setTempRating(rating);
+  };
+
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={i}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onRate={() => handleRating(i + 1)}
+            onHoverIn={() => handleTempRating(i + 1)}
+            onHoverOut={() => handleTempRating(0)}
+            color={color}
+            size={size}
+          />
+        ))}
+      </div>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
+    </div>
+  );
+};
--- my-movie-app/src/main.tsx
+++ my-movie-app/src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { StarRating } from "./StarRating";
+import { StarRating } from "./components/StarRating/StarRating";
 // import "./index.css";
 // import { App } from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <StarRating maxRating={5} />
+    <StarRating maxRating={10} color="#FE7743" size={24} />
+    <StarRating maxRating={20} color="#FF6363" size={20} />
   </StrictMode>,
 );
