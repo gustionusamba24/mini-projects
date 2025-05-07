331d114eb6bfc0b44965e0bcb76c939c12449ea7
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-05-07 15:01:41 +0700

    feat(my-movie-app): add useRef to select DOM element

--- my-movie-app/src/components/Nav/Search.tsx
+++ my-movie-app/src/components/Nav/Search.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useRef } from "react";
+
 type SearchProps = {
   query: string;
   setQuery: (value: string) => void;
 };
 
 export const Search = ({ query, setQuery }: SearchProps) => {
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const callback = (e) => {
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === "Enter") {
+        if (inputEl.current) {
+          inputEl.current.focus();
+        }
+        setQuery("");
+      }
+    };
+
+    document.addEventListener("keydown", callback);
+
+    return () => document.addEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -11,6 +32,7 @@ export const Search = ({ query, setQuery }: SearchProps) => {
       placeholder="Search for movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 };
